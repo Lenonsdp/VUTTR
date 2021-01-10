299fe355e1fbf784d33bf063edf491efdf0a9408
Lenon <lenon.paula@bling.com.br>
2021-01-10 10:07:16 -0300

    Finalização de testes de integração;

--- __tests__/integration/tool.test.js
+++ __tests__/integration/tool.test.js
@@ -2,22 +2,7 @@ import request from 'supertest';
 import app from '../../src/app';
 
 describe('Tools', () => {
-	it('Create Tool', async () => {
-		const response = await request(app)
-			.post('/tools')
-			.set('Authorization', `Bearer ${token}`)
-			.send({
-				title: "hotel",
-				link: "https://github.com/typicode/hotel",
-				description: "Local app manager. Start apps within your browser, developer tool with local .localhost domain and https out of the box.",
-				tags: ["node", "organizing", "webapps", "domain", "developer", "https", "proxy"]
-			});
-		expect(response.body).toHaveProperty('id', 'link', 'title', 'description', 'tags');
-	});
-});
-
-describe('Tools', () => {
-	it('Create Tool', async () => {
+	it('Create Tool with token invalid', async () => {
 		const response = await request(app)
 			.post('/tools')
 			.send({
@@ -27,5 +12,47 @@ describe('Tools', () => {
 				tags: ["node", "organizing", "webapps", "domain", "developer", "https", "proxy"]
 			});
 		expect(response.status).toBe(401);
+	});
+});
+
+describe('Tools', () => {
+	it('Create Tool with token valid', async () => {
+		const response = await request(app)
+			.post('/tools')
+			.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjEwMjgyMzIwfQ.JE_y9rBfdtpXvGzN2hMcsM42o53ls-D5Lgn9KxUwXNw`)
+			.send({
+				title: "hotel",
+				link: "https://github.com/typicode/hotel",
+				description: "Local app manager. Start apps within your browser, developer tool with local .localhost domain and https out of the box.",
+				tags: ["node", "organizing", "webapps", "domain", "developer", "https", "proxy"]
+			});
+		expect(response.status).toBe(201)
+	});
+});
+
+describe('Tools', () => {
+	it('Get tools', async () => {
+		const response = await request(app)
+			.get('/tools')
+			.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjEwMjgyMzIwfQ.JE_y9rBfdtpXvGzN2hMcsM42o53ls-D5Lgn9KxUwXNw`);
+		expect(response.status).toBe(200)
+	});
+});
+
+describe('Tools', () => {
+	it('Get tools by tag', async () => {
+		const response = await request(app)
+			.get('/tools/node')
+			.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjEwMjgyMzIwfQ.JE_y9rBfdtpXvGzN2hMcsM42o53ls-D5Lgn9KxUwXNw`);
+		expect(response.status).toBe(200)
+	});
+});
+
+describe('Tools', () => {
+	it('Delete tools by id', async () => {
+		const response = await request(app)
+			.delete('/tools/1')
+			.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjEwMjgyMzIwfQ.JE_y9rBfdtpXvGzN2hMcsM42o53ls-D5Lgn9KxUwXNw`);
+		expect(response.status).toBe(204)
 	});
 });
--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -14,6 +14,32 @@ describe('Users', () => {
 	});
 });
 
+describe('Users', () => {
+	it('Create user fail validation schema', async () => {
+		const response = await request(app)
+			.post('/users')
+			.send({
+				name: 'Lenon',
+				email: '',
+				password: '',
+			});
+		expect(response.status).toBe(400);
+	});
+});
+
+describe('Users', () => {
+	it('Create user fail user exists', async () => {
+		const response = await request(app)
+			.post('/users')
+			.send({
+				name: 'Lenon',
+				email: 'lenonsdp@hotmail.com',
+				password: '123456',
+			});
+		expect(response.status).toBe(400);
+	});
+});
+
 describe('Authentication', () => {
 	it('Create session', async () => {
 		const response = await request(app)
@@ -23,5 +49,36 @@ describe('Authentication', () => {
 				password: '123456',
 			});
 		expect(response.body).toHaveProperty('token');
+	});
+});
+
+describe('Update', () => {
+	it('Update user', async () => {
+		const response = await request(app)
+			.put('/users')
+			.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjEwMjgyMzIwfQ.JE_y9rBfdtpXvGzN2hMcsM42o53ls-D5Lgn9KxUwXNw`)
+			.send({
+				name: 'Lenon',
+				email: 'lenonsdp@hotmail.com',
+				oldPassword: '123456',
+				password: '654321',
+				confirmPassword: '654321',
+			});
+		expect(response.body).toHaveProperty('id', 'name', 'email');
+	});
+});
+
+describe('Update', () => {
+	it('Update user fail validation schema', async () => {
+		const response = await request(app)
+			.post('/users')
+			.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjEwMjgyMzIwfQ.JE_y9rBfdtpXvGzN2hMcsM42o53ls-D5Lgn9KxUwXNw`)
+			.send({
+				name: 'Lenon',
+				email: 'lenonsdp@hotmail.com',
+				oldPassword: '123456',
+				password: '654321',
+			});
+		expect(response.status).toBe(400);
 	});
 });

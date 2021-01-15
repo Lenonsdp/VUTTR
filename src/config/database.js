187bea027e4b6e85c08c93cb12517d50450bc013
Lenon <lenon.paula@bling.com.br>
2021-01-15 12:50:41 -0300

    Ajustes swagger e env database;

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
 	host: process.env.DB_HOST,
 	username: process.env.DB_NAME,
 	password: process.env.DB_PASSWORD,
-	database: 'vuttr',
+	database: process.env.DB_DATABASE,
 	storage: './__tests__/database.sqlite',
 	define: {
 		timestamp: true,

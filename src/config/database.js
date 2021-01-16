8fe570f9cbe9fd10445a5d6856d46cc494437541
Lenon <lenon.paula@bling.com.br>
2021-01-16 14:11:28 -0300

    Ajsute enviroment; Adiciona autenticação na rota de update user swagger;

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ require('dotenv').config({
 module.exports = {
 	dialect: process.env.DB_DIALECT || 'postgres',
 	host: process.env.DB_HOST,
-	username: process.env.DB_NAME,
+	username: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
 	storage: './__tests__/database.sqlite',
